7051a8212e008df1ffdd701d54c8384c515aeee5
Joanda Febrian <febrianjoanda@gmail.com>
2021-06-05 14:32:55 +0700

    feat: add view and controllers for all pages in prototype
    - New Training
    - Training History
    - Training Session
    - New Match
    - Match History
    - Match Session
    - Match Summary
    - Profile

---
+++ BadmintonTrainingSupport/Match/MatchSessionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MatchSessionViewController.swift
+//  BadmintonTrainingSupport
+//
+//  Created by Joanda Febrian on 05/06/21.
+//
+
+import UIKit
+
+class MatchSessionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BadmintonTrainingSupport/Match/MatchSummaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MatchSummaryViewController.swift
+//  BadmintonTrainingSupport
+//
+//  Created by Joanda Febrian on 05/06/21.
+//
+
+import UIKit
+
+class MatchSummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
