dd41aaaed4ea48c1551a3f529d06fc14c58928aa
Kevin Leon <kepin33@gmail.com>
2021-06-07 23:05:07 +0700

    mencoba menambahkan TrainingHistoryTabelViewCell

---
+++ BadmintonTrainingSupport/Training/Training Collection/TrainingHistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TrainingHistoryTableViewCell.swift
+//  BadmintonTrainingSupport
+//
+//  Created by Kevin Leon on 07/06/21.
+//
+
+import UIKit
+
+class TrainingHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
