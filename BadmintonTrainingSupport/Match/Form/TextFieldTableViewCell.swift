b06e9b591b303ca8955f388387d52f12340aac4f
Dhika Aditya Are <dhikaaditya1@gmail.com>
2021-06-08 14:00:08 +0700

    successfully created a xib and label text field for the "new match" page. But haven't tried it yet

---
+++ BadmintonTrainingSupport/Match/Form/TextFieldTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  TextFieldTableViewCell.swift
+//  BadmintonTrainingSupport
+//
+//  Created by Dhika Aditya Are on 07/06/21.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell, UITextViewDelegate {
+
+    @IBOutlet weak var labelField: UILabel!
+    @IBOutlet weak var nwField: UITextField!
+    
+    static let identifier = "TextFieldTableViewCell"
+    
+    static func nib() -> UINib {
+//        return UINib(nibName: identifier, bundle: nil)
+        return UINib(nibName: "TextFieldTableViewCell", bundle: nil)
+    }
+    
+    public func configure(with name: String) {
+        labelField.text = name
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        nwField.placeholder = "Enter text..."
+        //nwField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        print("\(nwField.text ?? "")")
+        return true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
