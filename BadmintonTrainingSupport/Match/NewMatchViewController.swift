05398ad3aa14290d2e4e2241cdba4b9e7ea8f429
Dhika Aditya Are <dhikaaditya1@gmail.com>
2021-06-08 17:05:25 +0700

    added and edited the Matchtype/MTsingle folder containing the "MTSingleTableviewCell" xib file for the "match type" feature. Not tested

--- BadmintonTrainingSupport/Match/NewMatchViewController.swift
+++ BadmintonTrainingSupport/Match/NewMatchViewController.swift
@@ -17,22 +17,55 @@ class NewMatchViewController: UIViewController {
         tableView.delegate = self
         
         tableView.register(TextFieldTableViewCell.nib(), forCellReuseIdentifier: TextFieldTableViewCell.identifier)
+        tableView.register(MTSingleTableViewCell.nib(), forCellReuseIdentifier: MTSingleTableViewCell.identifier)
     }
 
 }
 
 extension NewMatchViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 6
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+ /*
         if let fieldCell = tableView.dequeueReusableCell(withIdentifier: TextFieldTableViewCell.identifier, for: indexPath) as? TextFieldTableViewCell {
         
             return fieldCell
         }
+        
+        if let mtSingleCell = tableView.dequeueReusableCell(withIdentifier: MTSingleTableViewCell.identifier, for: indexPath) as? MTSingleTableViewCell {
+        
+            return mtSingleCell
+        }
+ */
+        
+        if indexPath.row > 2 {
+            print("ini gambar")
+        }
+        if indexPath.row > 5 {
+            let fieldCell = (tableView.dequeueReusableCell(withIdentifier: TextFieldTableViewCell.identifier, for: indexPath) as? TextFieldTableViewCell)!
+            
+            return fieldCell
+
+        }
+        
+        if indexPath.row > 7 {
+            print("ini segmented control")
+        }
+        
+ //MARK: Penanda Switch
+        if let mtSingleCell = tableView.dequeueReusableCell(withIdentifier: MTSingleTableViewCell.identifier, for: indexPath) as? MTSingleTableViewCell {
+        
+            return mtSingleCell
+        }
+        
         return UITableViewCell()
         
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
     
     
