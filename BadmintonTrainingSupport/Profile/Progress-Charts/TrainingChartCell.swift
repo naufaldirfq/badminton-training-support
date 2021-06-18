da23b178d25bbe1acf69e8ddd983713105892a44
truekevinic <kepin33@gmail.com>
2021-06-18 15:00:49 +0700

    menabahkan chart pot. kemudian cell untuk chart training

---
+++ BadmintonTrainingSupport/Profile/Progress-Charts/TrainingChartCell.swift
@@ -0,0 +1,23 @@
+//
+//  TrainingChartCell.swift
+//  BadmintonTrainingSupport
+//
+//  Created by Kevin Leon on 18/06/21.
+//
+
+import UIKit
+
+class TrainingChartCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
