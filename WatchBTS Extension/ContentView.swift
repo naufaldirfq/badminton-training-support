bd35ae41cb78499ddac6119f8703d2c53cd4aa6e
truekevinic <kepin33@gmail.com>
2021-06-18 13:20:56 +0700

    penambahan blank watchkit

---
+++ WatchBTS Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchBTS Extension
+//
+//  Created by Kevin Leon on 17/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchBTS Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchBTS Extension
+//
+//  Created by Kevin Leon on 17/06/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
